2950d371ea196909c6d354fe3ca001565fed3a95
oscarZ1 <118237839+oscarZ1@users.noreply.github.com>
2026-03-09 21:19:34 -0700

    Create tables_create.sql
    Blueprint of the tables we will use for the database.

---
+++ tables_create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE students (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    student_name TEXT NOT NULL, 
+    class TEXT
+);
+
+CREATE TABLE questions (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    student_id INT REFERENCES students(id),
+    time_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    questions TEXT
+); 
+
+CREATE TABLE comments (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    student_id INT REFERENCES students(id),
+    time_date TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    question_id INT REFERENcES students(id),
+    comments TEXT
+); 
