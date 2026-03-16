b5745ac2ab36b174e47b21fb1782a58f275276bb
oscarZ1 <118237839+oscarZ1@users.noreply.github.com>
2026-03-15 20:38:27 -0700

    Post Functions
    Added the post functions for questions and comments

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 import express from "express"; 
+import cors from "cors"; 
+import pg from "pg"; 
 
 // initilize express 
 const app = express(); 
 const port = 3000; 
 
+const corsOptions = {
+    origin: ["http://localhost:5173"]
+};
+
+const client = new pg.Client({
+  user: "postgres", 
+  password: "oscarpostgres", 
+  host: "localhost", 
+  database: "world", 
+  port: 5432
+});
+
+client.connect(); 
+
 //middleware 
 app.use(express.json()); // For parsing JSON data (used for API requests)
 app.use(express.urlencoded({ extended: true })); // For parsing form data (used for standard html)
+app.use(cors(corsOptions)); 
 
 app.get("/", (res, req) => {
     res.json({
@@ -14,12 +31,48 @@ app.get("/", (res, req) => {
     })
 });
 
-app.post("/post", (res, req) => {
+async function findIndex(index) {
+    
+};
 
+app.post("/api/questions", async (res, req) => {
+    const { student_id, class_id, content } = req.body;
+
+    try {
+        const newQuestion = await client.query(
+        "INSERT INTO questions (student_id, class_id, content) VALUES ($1, $2, $3) RETURNING *",
+        [student_id, class_id, content]
+        );
+
+        res.status(201).json(newQuestion.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Server error while posting" });
+    }
 }); 
 
-app.post("/post", (res, req) => {
+app.post("/api/comments", async (res, req) => {
+    const { student_id, class_id, content } = req.body;
 
+    try {
+        const newQuestion = await client.query(
+        "INSERT INTO comments (student_id, class_id, content) VALUES ($1, $2, $3) RETURNING *",
+        [student_id, class_id, content]
+        );
+
+        res.status(201).json(newQuestion.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Server error while posting" });
+    }
+});
+
+app.delete("/api/questions", (res, req) => {
+    
+}); 
+
+app.delete("/api/comments", (res, req) => {
+    
 }); 
 
 app.listen(port, () => {
--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 import express from "express" 
-import axios from "axios" 
-import cors from "cors"
+
 
 const app = express();
 const port = 3000; 
 
-const corsOptions = {
-    origin: ["http://localhost:5173"]
-};
-
 // middleware 
 app.use(express.static("./client/src/pages")); // gives us access to the pages folder to access the html and css files
 app.use(express.json()); // For parsing JSON data (used for API requests)
-app.use(express.urlencoded({ extended: true })); // For parsing form data (used for standard html)
-app.use(cors(corsOptions)); 
+
 
 // gets the homepage 
 // loads up 
